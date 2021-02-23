418ef8dd1cc109fd817fe11223cc18ceaaa0b204
robinlrx <robin@monkey-lab.io>
2021-02-23 01:48:39 +0100

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,43 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	plugins: ['es-beautifier'],
+	extends: [
+		'plugin:vue/essential',
+		'plugin:es-beautifier/standard',
+		'@vue/airbnb',
+	],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'linebreak-style': 'off',
+		indent: ['error', 'tab'],
+		// vue html quotes work with js-beautify-html
+		'vue/html-quotes': ['error', 'single'],
+		// vue html tab work with js-beautify-html
+		'vue/html-indent': [
+			'error',
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+				alignAttributesVertically: true,
+				ignores: [],
+			},
+		],
+		// vue html unsued components
+		'vue/no-unused-components': [
+			'error',
+			{
+				ignoreWhenBindingPresent: false,
+			},
+		],
+	},
 };
